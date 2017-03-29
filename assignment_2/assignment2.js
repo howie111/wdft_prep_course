2724c09c4fb301153f96568a829a8685044d2aa5
Howie Wang <wanghowie@gmail.com>
2017-03-29 17:24:20 -0400

    assigment2 done

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,6 +2,10 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
+for (var i = 1; i<=100; i++)
+{
+console.log(i);
+}
 
 
 
@@ -17,3 +21,41 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+for(var i=1;i <=7;i++)
+{
+    outputTrangle(i);
+}
+
+
+
+function outputTrangle(cnt)
+{
+switch (cnt){
+ case 1:
+   console.log('#')
+   break;
+ case 2:
+   console.log('##')
+   break;
+ case 3:
+   console.log('###')
+   break;
+ case 4:
+   console.log('####')
+   break;
+ case 5:
+   console.log('#####')
+   break;
+ case 6:
+   console.log('######')
+   break;
+ case 7:
+   console.log('#######')
+   break;
+
+
+}
+ 
+
+}
